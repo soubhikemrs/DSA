d691ccb363ecefb8710d65ae1de8e3c09d406dda
soubhikemrs <soubhik.work@zohomail.in>
2026-04-12 04:12:58 +0530

    Time: 7 ms (41.46%), Space: 20.5 MB (36.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,14 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        last_idx = -1 #stores the index of last encountered non-zero element so far
-        i = 0 #will use it to traverse the array
-        while i < len(nums):
-            if nums[i] != 0:
-                last_idx += 1
-                nums[last_idx], nums[i] = nums[i], nums[last_idx]
-            i += 1
-        '''automatically as we swap all non-zero elements to the left most side, 
-        the right size will be filled with 0s'''
-        
+        # A different way to solve using python sort function
+        nums.sort(key=lambda x : x==0)
 
